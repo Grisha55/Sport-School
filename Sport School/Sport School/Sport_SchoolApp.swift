5b2f3deb0a1a702a40b21540f35f6b3361385c75
Grigory <grishavinyar@icloud.com>
2022-08-15 15:37:42 +0300

    Initial commit

---
+++ Sport School/Sport School/Sport_SchoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sport_SchoolApp.swift
+//  Sport School
+//
+//  Created by Григорий Виняр on 15/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Sport_SchoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
